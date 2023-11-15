db44b5a76b8d3446886cc62c9470156da94e77dd
gepetridis <gepetridis@uth.gr>
2023-11-15 14:58:55 +0200

    c1 - Initial Empty Program

---
+++ rainfall.f95
@@ -0,0 +1,20 @@
+program rainfall
+implicit none
+
+!integer::i
+!character(20)::town(5)
+!real::rain(5)
+
+!open(1,file='data.txt')
+
+!read(1,*)
+!do i=1,5
+! read(1,*) town(i),rain(i)
+!end do 
+
+!close(1)
+
+!print*,town
+!print*,rain
+
+end program
